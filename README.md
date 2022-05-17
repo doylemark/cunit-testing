df3cd2a4965335472b0acd441536ec459049e7fb
Mark Doyle <39464373+doylemark@users.noreply.github.com>
2022-05-17 17:41:07 +0100

    add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Running tests:
+
+Run `./build.sh && ./dist/testing` to run tests
--- tests.c
+++ tests.c
@@ -48,6 +48,6 @@ int main()
     CU_basic_set_mode(CU_BRM_VERBOSE);
     CU_basic_run_tests();
     CU_cleanup_registry();
-    printf("%lf\n", average((double[]){2, 2, 2}, 3));
+
     return 0;
 }
