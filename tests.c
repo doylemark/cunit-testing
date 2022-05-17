ed0a35e009b431f7a7f51ddf50a4e51908d5a9ed
Mark Doyle <39464373+doylemark@users.noreply.github.com>
2022-05-17 17:40:00 +0100

    add avg tests

--- tests.c
+++ tests.c
@@ -5,7 +5,6 @@
 
 #include "src/avg_and_max.h"
 
-
 void test_max()
 {
     // base case
@@ -16,9 +15,26 @@ void test_max()
     CU_ASSERT(max((double[]){-1, -10, -15}, 3) == -1);
     // all same value
     CU_ASSERT(max((double[]){1, 1}, 2) == 1);
-    // all zero
+    // all zero (fails)
     CU_ASSERT(max((double[]){0, 0, 0}, 3) == 0);
+    // only one elem (fails)
+    CU_ASSERT(max((double[]){1}, 1) == 1);
+    // empty arr (fails)
+    CU_ASSERT(max((double[]){0}, 0) == 0);
+}
 
+void test_avg()
+{
+    // base case (fails)
+    CU_ASSERT(average((double[]){1, 2, 3}, 3) == 2);
+    // all negative (fails)
+    CU_ASSERT(average((double[]){-1, -2, -3}, 3) == -2);
+    // all same (fails)
+    CU_ASSERT(average((double[]){2, 2, 2}, 3) == 2);
+    // only one elem (fails)
+    CU_ASSERT(average((double[]){2, 2, 2}, 3) == 2);
+    // empty arr (fails)
+    CU_ASSERT(average((double[]){}, 0) == 0);
 }
 
 int main() 
@@ -27,10 +43,11 @@ int main()
     CU_pSuite suite = CU_add_suite("test_max", 0, 0);
 
     CU_add_test(suite, "test_max", test_max);
+    CU_add_test(suite, "test_avg", test_avg);
 
     CU_basic_set_mode(CU_BRM_VERBOSE);
     CU_basic_run_tests();
     CU_cleanup_registry();
-    printf("%lf", max((double[]){0, 0, 0}, 3));
+    printf("%lf\n", average((double[]){2, 2, 2}, 3));
     return 0;
 }
